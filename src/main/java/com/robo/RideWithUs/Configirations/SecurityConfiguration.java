57516e8a418286e585e68cca60ed7184e4317914
= <=>
2026-01-05 13:44:41 +0530

    Fix CORS 403 issue

--- src/main/java/com/robo/RideWithUs/Configirations/SecurityConfiguration.java
+++ src/main/java/com/robo/RideWithUs/Configirations/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.robo.RideWithUs.Configirations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -28,6 +29,7 @@ public class SecurityConfiguration {
 	            .cors(cors -> {})
 	            .authorizeHttpRequests(auth -> auth
 	                .requestMatchers("/auth/**").permitAll()
+	                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() 
 	                .requestMatchers("/customer/**").hasRole("CUSTOMER")
 	                .requestMatchers("/driver/**").hasRole("DRIVER")
 	                .anyRequest().authenticated()
